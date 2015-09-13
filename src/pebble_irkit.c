33c6ac2b10b17d83f79ce12c8a80b136967b2f33
makoto_kw <makoto.kw@gmail.com>
2015-09-14 02:25:50 +0900

    Supported Pebble Time

--- src/pebble_irkit.c
+++ src/pebble_irkit.c
@@ -1,8 +1,14 @@
 #include <pebble.h>
 
 static Window *s_window;
+static GRect s_window_bounds;
 static MenuLayer *s_menu_layer;
 static TextLayer *s_text_layer;
+
+#ifdef PBL_SDK_3
+static StatusBarLayer *s_status_bar;
+static Layer *s_battery_layer;
+#endif
 
 static uint16_t s_num_command;
 static char **s_command_names;
@@ -61,7 +67,14 @@ void commands_init_array() {
 // Toast: popup text
 
 static void toast_init() {
-  s_text_layer = text_layer_create(GRect(0, 130, 144, 25));
+  const int16_t toast_height = 25;
+  GRect toast_bounds = GRect(
+    s_window_bounds.origin.x,
+    s_window_bounds.size.h - toast_height,
+    s_window_bounds.size.w,
+    toast_height
+  );
+  s_text_layer = text_layer_create(toast_bounds);
   text_layer_set_background_color(s_text_layer, GColorBlack);
   text_layer_set_text_color(s_text_layer, GColorWhite);
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
@@ -159,20 +172,20 @@ static void message_init() {
 // --------------------------------------------------------
 // Menu
 
-static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *context) {
   return NUM_MENU_SECTIONS;
 }
 
-static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *context) {
   return s_num_command;
 }
 
-static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *context) {
   // This is a define provided in pebble.h that you may use for the default height
   return MENU_CELL_BASIC_HEADER_HEIGHT;
 }
 
-static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *context) {
   switch (section_index) {
     case 0:
       menu_cell_basic_header_draw(ctx, cell_layer, "Commands");
@@ -180,36 +193,15 @@ static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, ui
   }
 }
 
-static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *context) {
   switch (cell_index->section) {
     case 0:
       menu_cell_basic_draw(ctx, cell_layer, s_command_names[cell_index->row], NULL, NULL);
-//       menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "With a subtitle", NULL);
-
-    //   // Use the row to specify which item we'll draw
-    //   switch (cell_index->row) {
-    //     case 0:
-    //       // This is a basic menu item with a title and subtitle
-    //       menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "With a subtitle", NULL);
-    //       break;
-      //
-    //     case 1:
-    //       // This is a basic menu icon with a cycling icon
-    //       menu_cell_basic_draw(ctx, cell_layer, "Icon Item", "Select to cycle", menu_icons[current_icon]);
-    //       break;
-      //
-    //     case 2:
-    //       // Here we use the graphics context to draw something different
-    //       // In this case, we show a strip of a watchface's background
-    //       graphics_draw_bitmap_in_rect(ctx, menu_background,
-    //           (GRect){ .origin = GPointZero, .size = layer_get_frame((Layer*) cell_layer).size });
-    //       break;
-    //   }
       break;
   }
 }
 
-void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
+void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "MenuSelected section:%d row:%d", cell_index->section, cell_index->row);
   if (cell_index->section == 0) {
     send_selected_command(cell_index->row);
@@ -218,8 +210,17 @@ void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *da
 
 static void menu_init(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-  s_menu_layer = menu_layer_create(bounds);
+#ifdef PBL_SDK_3
+  GRect menu_bound = GRect(
+    s_window_bounds.origin.x,
+    s_window_bounds.origin.y + STATUS_BAR_LAYER_HEIGHT,
+    s_window_bounds.size.w,
+    s_window_bounds.size.h - STATUS_BAR_LAYER_HEIGHT
+  );
+  s_menu_layer = menu_layer_create(menu_bound);
+#else
+  s_menu_layer = menu_layer_create(s_window_bounds);
+#endif
 
   menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks){
     .get_num_sections = menu_get_num_sections_callback,
@@ -232,6 +233,10 @@ static void menu_init(Window *window) {
 
   menu_layer_set_click_config_onto_window(s_menu_layer, window);
   layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+
+#ifdef PBL_COLOR
+  menu_layer_set_highlight_colors(s_menu_layer, GColorCyan, GColorBlack);
+#endif
 }
 
 static void menu_deinit() {
@@ -240,35 +245,49 @@ static void menu_deinit() {
 }
 
 // --------------------------------------------------------
-//
+// Status Bar
 
-// static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-//   text_layer_set_text(text_layer, "Select");
-// }
-//
-// static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-//   text_layer_set_text(text_layer, "Up");
-// }
-//
-// static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-//   text_layer_set_text(text_layer, "Down");
-// }
-//
-// static void click_config_provider(void *context) {
-//   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-//   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-//   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-// }
+#ifdef PBL_SDK_3
+static void battery_proc(Layer *layer, GContext *ctx) {
+  // Emulator battery meter on Aplite
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_draw_rect(ctx, GRect(126, 4, 14, 8));
+  graphics_draw_line(ctx, GPoint(140, 6), GPoint(140, 9));
 
+  BatteryChargeState state = battery_state_service_peek();
+  int width = (int)(float)(((float)state.charge_percent / 100.0F) * 10.0F);
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, GRect(128, 6, width, 4), 0, GCornerNone);
+}
+#endif
 
+static void status_bar_init(Window *window) {
+#ifdef PBL_SDK_3
+  Layer *window_layer = window_get_root_layer(window);
+  GRect status_bar_bounds = GRect(s_window_bounds.origin.x, s_window_bounds.origin.y, s_window_bounds.size.w, STATUS_BAR_LAYER_HEIGHT);
+
+  // Set up the status bar last to ensure it is on top of other Layers
+  s_status_bar = status_bar_layer_create();
+  layer_add_child(window_layer, status_bar_layer_get_layer(s_status_bar));
+
+  // Show legacy battery meter
+  s_battery_layer = layer_create(status_bar_bounds);
+  layer_set_update_proc(s_battery_layer, battery_proc);
+  layer_add_child(window_layer, s_battery_layer);
+#endif
+}
 
 // --------------------------------------------------------
 // Window
 
 static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  s_window_bounds = layer_get_bounds(window_layer);
+
   message_init();
-  toast_init();
   menu_init(window);
+  status_bar_init(window);
+  toast_init();
   toast_show("loading...");
 }
 
